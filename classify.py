2732f1613ee14c0ce1cc49ae76cdfc5ac48f16d9
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2019-12-31 16:21:39 -0500

    run autopep8 on everything

--- classify.py
+++ classify.py
@@ -8,15 +8,15 @@ model = fasttext.load_model("cc.model")
 transactionData = get_data(sys.argv[1])
 
 for _, sheet in transactionData.items():
-  for row in sheet[1:]:
-    if len(row) == 0:
-      continue
+    for row in sheet[1:]:
+        if len(row) == 0:
+            continue
 
-    p = model.predict(format_row(row, False))
+        p = model.predict(format_row(row, False))
 
-    if len(row) < 6:
-      row.append('')
+        if len(row) < 6:
+            row.append('')
 
-    row[5] = p[0][0].replace('__label__', '')
+        row[5] = p[0][0].replace('__label__', '')
 
 save_data(sys.argv[1], transactionData)
--- train.py
+++ train.py
@@ -4,7 +4,8 @@ import fasttext
 import random
 import sys
 
-VERIFY_FACTOR = 8 # 80% of input should be treated as training, 20% should be treated as verification
+# 80% of input should be treated as training, 20% should be treated as verification
+VERIFY_FACTOR = 8
 
 transactionData = get_data(sys.argv[1])
 
@@ -13,32 +14,32 @@ trainFile = open("/tmp/cctransactions.train", "w")
 v = []
 
 for _, sheet in transactionData.items():
-  for row in sheet[1:]:
-    if len(row) < 6:
-      continue
+    for row in sheet[1:]:
+        if len(row) < 6:
+            continue
 
-    if row[5] == '':
-      continue
+        if row[5] == '':
+            continue
 
-    v.append(format_row(row, True))
+        v.append(format_row(row, True))
 
 trainFile = open("/tmp/cctransactions.train", "w")
 
 random.shuffle(v)
 
 for row in v[:(len(v)//10)*8]:
-  print(row, file = trainFile)
+    print(row, file=trainFile)
 
 trainFile.close()
 
 verifyFile = open("/tmp/cctransactions.verify", "w")
 
 for row in v[(len(v)//10)*8:]:
-  print(row, file = verifyFile)
+    print(row, file=verifyFile)
 
 verifyFile.close()
 
-model = fasttext.train_supervised(input="/tmp/cctransactions.train", epoch=50, lr=0.8, wordNgrams=2)
+model = fasttext.train_supervised(
+    input="/tmp/cctransactions.train", epoch=50, lr=0.8, wordNgrams=2)
 print(model.test("/tmp/cctransactions.verify"))
 model.save_model("cc.model")
-
